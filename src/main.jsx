d6c87f9c11a0b69079d2890a7d12469733a464fd
Your Name <73336938+KD-96@users.noreply.github.com>
2025-05-02 12:13:10 +0530

    test

--- src/main.jsx
+++ src/main.jsx
@@ -9,17 +9,17 @@ const allowedDomains = ["localhost", "iframetester.com"];
 const allowedParentDomain = ["iframetester.com"];
 const currentDomain = window.location.hostname;
 
-if (window.top === window.self) {
-  document.body.innerHTML = "<h2>Direct access not allowed</h2>";
-  throw new Error("Blocked: Direct access");
-}
+// if (window.top === window.self) {
+//   document.body.innerHTML = "<h2>Direct access not allowed</h2>";
+//   throw new Error("Blocked: Direct access");
+// }
 
-// Block iframe from unknown domains
-const referrer = document.referrer;
-if (!referrer.includes(allowedParentDomain)) {
-  document.body.innerHTML = "<h2>Unauthorized embedding</h2>";
-  throw new Error("Blocked: Unauthorized embedding");
-}
+// // Block iframe from unknown domains
+// const referrer = document.referrer;
+// if (!referrer.includes(allowedParentDomain)) {
+//   document.body.innerHTML = "<h2>Unauthorized embedding</h2>";
+//   throw new Error("Blocked: Unauthorized embedding");
+// }
 
 if (!allowedDomains.some(domain => currentDomain.includes(domain))) {
   document.body.innerHTML = "<h2>Access denied</h2>";
