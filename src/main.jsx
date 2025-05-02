3957c8dc531f26f8bf0c087a61d9df6715a5ebec
Your Name <73336938+KD-96@users.noreply.github.com>
2025-05-02 12:06:55 +0530

    test - allowed doms

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,27 @@ import App from './App.jsx'
 import { ThemeProvider, CssBaseline } from '@mui/material'
 import Theme from './themes/Theme.jsx'
 
+const allowedDomains = ["localhost"];
+const allowedParentDomain = ["https://iframetester.com/"];
+const currentDomain = window.location.hostname;
+
+if (window.top === window.self) {
+  document.body.innerHTML = "<h2>Direct access not allowed</h2>";
+  throw new Error("Blocked: Direct access");
+}
+
+// Block iframe from unknown domains
+const referrer = document.referrer;
+if (!referrer.includes(allowedParentDomain)) {
+  document.body.innerHTML = "<h2>Unauthorized embedding</h2>";
+  throw new Error("Blocked: Unauthorized embedding");
+}
+
+if (!allowedDomains.some(domain => currentDomain.includes(domain))) {
+  document.body.innerHTML = "<h2>Access denied</h2>";
+  throw new Error("Unauthorized domain");
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider theme={Theme}>
